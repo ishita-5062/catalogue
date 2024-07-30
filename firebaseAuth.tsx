67a3080e4ebebefbb2889901ddfb0ca4d2b5c9ce
Saachi Bansal <121082132+SaachiBansal@users.noreply.github.com>
2024-07-30 17:33:29 +0530

    swipe feature logic pt 3

--- firebaseAuth.tsx
+++ firebaseAuth.tsx
@@ -4,12 +4,12 @@ import {getAuth} from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "firebase_domain",
+  apiKey: "AIzaSyB-AewnD40RrKDFODxuA38-7LrEuvMu1bw",
+  authDomain: "catalogue-a9b2f.firebaseapp.com",
   projectId: "catalogue-a9b2f",
-  storageBucket: "xxx",
-  messagingSenderId: "xxx",
-  appId: "add_id"
+  storageBucket: "catalogue-a9b2f.appspot.com",
+  messagingSenderId: "1077369271124",
+  appId: "1:1077369271124:web:6ac1773f241c6727cd4ec1"
 };
 
 // Initialize Firebase
